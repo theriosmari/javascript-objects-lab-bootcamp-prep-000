8ad4a63ba0c6f8da66bb7dd5379cbac82182fe52
Mariel Ríos <marielbrios09@gmail.com>
2017-12-31 08:46:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var recipes = { dessert: 'cookies' }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  return Object.assign({}, object, { [key]: value })
 }
