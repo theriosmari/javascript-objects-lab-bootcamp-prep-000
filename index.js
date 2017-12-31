24fb5c060103e75c82bc7680aca61925283373ae
Mariel Ríos <marielbrios09@gmail.com>
2017-12-31 08:49:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function deleteFromObjectByKey(object, key)
+function deleteFromObjectByKey(object, key) {
+  
+}
